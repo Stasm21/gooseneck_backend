20df7317e4943fe0dfd9145ccc023766f2e004b9
Stas <stasm21@gmail.com>
2022-02-11 11:14:35 -0800

    post request successful

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-// var mysql = require("mysql");
-// const express = require("express");
-// const app = express();
-
-// const connection = mysql.createConnection({
-//     host: "127.0.0.1",
-//     user: "root",
-//     password: "password",
-//     database: 'gooseneck_Database'
-// });
-
-// connection.connect(function (err) {
-//     if (err) {
-//         return console.error("error: " + err.message);
-//     }
-
-//     console.log("Connected to the MySQL server.");
-// });
 
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const mysql = require("mysql");
 
 const db = mysql.createPool({
@@ -28,9 +12,20 @@ const db = mysql.createPool({
     database: "gooseneck_Database",
 });
 
-app.get("/", (req, res) => {
-    
-});
+app.use(cors());
+app.use(express.json)
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('api/insert', (req, res) => {
+
+    const topic = req.body.topic
+    const question = req.body.question
+
+    const sqlInsert = "INSERT INTO questions (topic, question) VALUES (?, ?)";
+    db.query(sqlInsert, [topic, question], (err, result) => {
+        console.log(result)
+    })
+})
 
 app.listen(3001, () => {
     console.log("running on port 3001");
