f45d40fcd51bde935bf13f833f9efdaf68fb81d4
Stas <stasm21@gmail.com>
2022-02-13 23:06:13 -0800

    created pool

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5001;
 
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
